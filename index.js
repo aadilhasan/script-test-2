d4808e656d6e079f98db4aeb2d4a2ad673253e55
Aadil Hasan <aadil.hasan07@gmail.com>
2020-05-13 01:38:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-!(function(e, t) {
+!(function (e, t) {
   "object" == typeof exports && "undefined" != typeof module
-    ? t(exports, require("react"))
+    ? (module.exports = t(require("react")))
     : "function" == typeof define && define.amd
-    ? define(["exports", "react"], t)
-    : t(((e = e || self).extension = {}), e.react);
-})(this, function(e, t) {
-  (t = t && Object.prototype.hasOwnProperty.call(t, "default") ? t.default : t),
-    (e.App = function() {
-      return t.createElement("div", null, " Hello From React ");
-    });
+    ? define(["react"], t)
+    : ((e = e || self).reactAsyncPopup = t(e.react));
+})(this, function (e) {
+  var t = "default" in e ? e.default : e;
+  return function () {
+    var n = e.useState(0),
+      u = n[0],
+      r = n[1];
+    return t.createElement(
+      "div",
+      null,
+      t.createElement("h2", null, " ", u, " "),
+      t.createElement(
+        "button",
+        {
+          onClick: function () {
+            return r(u + 1);
+          },
+        },
+        " ++ count "
+      ),
+      t.createElement(
+        "button",
+        {
+          onClick: function () {
+            return r(u - 1);
+          },
+        },
+        " -- count "
+      )
+    );
+  };
 });
